fd8e3569d577a18e33cce4efb546f7b66ed3f839
Theonrex <theonrex1@gmail.com>
2022-06-21 10:25:35 +0100

    commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// Addition
+let a = 20 + 50;
+console.log(a);
+
+// Subtraction
+let b = 55 - 33;
+console.log(b);
+// Multiplication
+let x = 100 * 90;
+console.log(x);
+
+// Division
+let f = 10 / 5;
+console.log(f)
